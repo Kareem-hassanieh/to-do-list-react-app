6e8f84507e10adf96817256021cef2cf3080359f
Kareem-hassanieh <kareemalh19@gmail.com>
2024-10-22 13:24:28 +0300

    UI of the header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <div className='mt-[100px]'>hi</div>
+    <>
+     <div className=''>
+     <Header />
+
+     </div>
+     
+    </>
+   
   )
 }
 
